4662525f0c9a3de35fcea9745d68f9634bc085d4
Shen Jin <sjin@Shens-Air.corp.kik.com>
2016-04-18 11:58:58 -0400

    testing stuff

---
+++ api/message.py
@@ -0,0 +1,23 @@
+from untitled3 import kik, app
+from flask import request, Response
+from kik.messages import messages_from_json, TextMessage
+
+
+@app.route('/receive', methods=['POST'])
+def receive():
+    if not kik.verify_signature(request.headers.get('X-Kik-Signature'), request.get_data()):
+        return Response(status=403)
+
+    messages = messages_from_json(request.json['messages'])
+
+    for message in messages:
+        if isinstance(message, TextMessage):
+            kik.send_messages([
+                TextMessage(
+                    to=message.from_user,
+                    chat_id=message.chat_id,
+                    body=message.body
+                )
+            ])
+
+        return Response(status=200)
--- untitled3.py
+++ untitled3.py
@@ -1,12 +1,23 @@
+from importlib import import_module
+import os
+import sys
 from flask import Flask
 
+from kik import KikApi, Configuration
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '.'))
+
+USER_NAME = os.environ["USER_NAME"]
+API_KEY = os.environ["API_KEY"]
+
 app = Flask(__name__)
+kik = KikApi(USER_NAME, API_KEY)
 
+kik.set_configuration(Configuration(webhook='https://1d316085.ngrok.io/recieve'))
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+for file_name in os.listdir('{}/api'.format(os.path.dirname(os.path.realpath(__file__)))):
+    if not file_name.startswith('.') and file_name.endswith('.py') and file_name != '__init__.py':
+        import_module('api.{}'.format(file_name[:-3]))
 
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(port=8080, debug=True)
