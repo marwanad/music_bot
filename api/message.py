cf4c9463088dce3543cc79cb8ae643769fc1ce50
rfarmaha <ramandeep.f@gmail.com>
2016-04-18 13:14:30 -0400

    init

--- api/message.py
+++ api/message.py
@@ -1,10 +1,19 @@
-from untitled3 import kik, app
-from flask import request, Response
+from flask import Blueprint, request, Response
 from kik.messages import messages_from_json, TextMessage
+import os
+from kik import KikApi, Configuration
+
+USER_NAME = os.environ["USER_NAME"]
+API_KEY = os.environ["API_KEY"]
+WEB_HOOK = os.environ["FLASK_WEBHOOK"]
+
+api = Blueprint('api', __name__)
+kik = KikApi(USER_NAME, API_KEY)
 
 
-@app.route('/receive', methods=['POST'])
+@api.route('/receive', methods=['POST'])
 def receive():
+    kik.set_configuration(Configuration(webhook=WEB_HOOK + '/receive'))
     if not kik.verify_signature(request.headers.get('X-Kik-Signature'), request.get_data()):
         return Response(status=403)
 
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from importlib import import_module
+from api.message import api
 import os
 import sys
 from flask import Flask
@@ -9,11 +10,13 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '.'))
 
 USER_NAME = os.environ["USER_NAME"]
 API_KEY = os.environ["API_KEY"]
+WEB_HOOK = os.environ["FLASK_WEBHOOK"]
 
 app = Flask(__name__)
+app.register_blueprint(api)
 kik = KikApi(USER_NAME, API_KEY)
 
-kik.set_configuration(Configuration(webhook='https://1d316085.ngrok.io/recieve'))
+kik.set_configuration(Configuration(webhook=WEB_HOOK + '/receive'))
 
 for file_name in os.listdir('{}/api'.format(os.path.dirname(os.path.realpath(__file__)))):
     if not file_name.startswith('.') and file_name.endswith('.py') and file_name != '__init__.py':
@@ -21,3 +24,5 @@ for file_name in os.listdir('{}/api'.format(os.path.dirname(os.path.realpath(__f
 
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
+
+__all__ = ['app']
