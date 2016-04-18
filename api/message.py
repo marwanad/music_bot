5a8e8a5617b04aa4d1354b73223044ae8839bf8b
Shen Jin <sjin@Shens-Air.corp.kik.com>
2016-04-18 13:58:39 -0400

    srs for start-messaging

--- api/message.py
+++ api/message.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, request, Response
-from kik.messages import messages_from_json, TextMessage
+from flask import Blueprint, request, Response, redirect, url_for
+from kik.messages import messages_from_json, TextMessage, StartChattingMessage, SuggestedResponseKeyboard, TextResponse
 import os
 from kik import KikApi, Configuration
 
@@ -20,13 +20,26 @@ def receive():
     messages = messages_from_json(request.json['messages'])
 
     for message in messages:
-        if isinstance(message, TextMessage):
-            kik.send_messages([
-                TextMessage(
-                    to=message.from_user,
-                    chat_id=message.chat_id,
-                    body=message.body
-                )
-            ])
+        # if isinstance(message, StartChattingMessage):
+        return redirect(url_for('.intro', to=message.from_user, chat_id=message.chat_id), code=302)
+        # return Response(status=200)
 
-        return Response(status=200)
+
+@api.route('/intro', methods=['GET'])
+def intro():
+    to = request.args.get('to')
+    chat_id = request.args.get('chat_id')
+    srs = [TextResponse(body=sr) for sr in ['ABC', 'DEF', 'HIJ', 'LMN', 'OPQ', 'RST']]
+    body = 'Hi you reached the intro stage, tap a sr for more options :+1:'
+
+    if to and chat_id:
+        kik.send_messages([
+            TextMessage(
+                to=to,
+                chat_id=chat_id,
+                body=body,
+                keyboards=[SuggestedResponseKeyboard(responses=srs)]
+            )
+        ])
+    return Response(status=200)
+
