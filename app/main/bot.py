ecbcf6d09bccee0f230ab0dd453468e2014709af
Marwan Ahmed <marwan.ahmed@kik.com>
2016-04-19 11:48:46 -0400

    blueprint

--- app/main/bot.py
+++ app/main/bot.py
@@ -25,7 +25,7 @@ def receive():
                     WubbleMessage(
                     to=message.from_user,
                     chat_id=message.chat_id,
-                    url=url_for("music_player")
+                    url=url_for("main.music_player")
                 )
                     ])
         return Response(status=200)
