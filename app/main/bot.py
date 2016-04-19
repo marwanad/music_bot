c1827be4309099527e1365fff8ae9d4cf9134224
Marwan Ahmed <marwan.ahmed@kik.com>
2016-04-19 14:27:23 -0400

    add spotipy again

--- app/main/bot.py
+++ app/main/bot.py
@@ -34,48 +34,48 @@ def receive():
                     url=url_for("main.music_player", id="e001676375ea2b4807cee2f98b51f2f3fe0d109b",_external=True)
                 )
                     ])
-                return Response(status=200)
+        #         return Response(status=200)
 
-            fn = srs.srs.get(message.body.lower())
-            if not fn:
-                Handler.handle_fallback(to, chat_id)
-                return Response(status=200)
-            getattr(Handler, fn)(to, chat_id)
-        else:
-            Handler.handle_fallback(to, chat_id)
+        #     fn = srs.srs.get(message.body.lower())
+        #     if not fn:
+        #         Handler.handle_fallback(to, chat_id)
+        #         return Response(status=200)
+        #     getattr(Handler, fn)(to, chat_id)
+        # else:
+        #     Handler.handle_fallback(to, chat_id)
         return Response(status=200)
 
 
-class Handler(object):
-    @staticmethod
-    def handle_intro(to, chat_id):
-        body = 'Hi you reached the intro stage, tap a sr for more options :+1:'
-        Responder.send_text_response(to, chat_id, body)
+# class Handler(object):
+#     @staticmethod
+#     def handle_intro(to, chat_id):
+#         body = 'Hi you reached the intro stage, tap a sr for more options :+1:'
+#         Responder.send_text_response(to, chat_id, body)
 
-    @staticmethod
-    def handle_start_quiz(to, chat_id):
-        body = 'start quiz'
-        Responder.send_text_response(to, chat_id, body, keyboards=[MAIN_SR])
+#     @staticmethod
+#     def handle_start_quiz(to, chat_id):
+#         body = 'start quiz'
+#         Responder.send_text_response(to, chat_id, body, keyboards=[MAIN_SR])
 
-    @staticmethod
-    def handle_custom_track(to, chat_id):
-        body = 'custom track'
-        Responder.send_text_response(to, chat_id, body, keyboards=[MAIN_SR])
+#     @staticmethod
+#     def handle_custom_track(to, chat_id):
+#         body = 'custom track'
+#         Responder.send_text_response(to, chat_id, body, keyboards=[MAIN_SR])
 
-    @staticmethod
-    def handle_share(to, chat_id):
-        body = 'share'
-        Responder.send_text_response(to, chat_id, body, keyboards=[MAIN_SR])
+#     @staticmethod
+#     def handle_share(to, chat_id):
+#         body = 'share'
+#         Responder.send_text_response(to, chat_id, body, keyboards=[MAIN_SR])
 
-    @staticmethod
-    def handle_settings(to, chat_id):
-        body = 'settings'
-        Responder.send_text_response(to, chat_id, body, keyboards=[MAIN_SR])
+#     @staticmethod
+#     def handle_settings(to, chat_id):
+#         body = 'settings'
+#         Responder.send_text_response(to, chat_id, body, keyboards=[MAIN_SR])
 
-    @staticmethod
-    def handle_fallback(to, chat_id):
-        body = 'fallback'
-        Responder.send_text_response(to, chat_id, body, keyboards=[MAIN_SR])
+#     @staticmethod
+#     def handle_fallback(to, chat_id):
+#         body = 'fallback'
+#         Responder.send_text_response(to, chat_id, body, keyboards=[MAIN_SR])
 
 
 @main.route('/musicplayer/<id>', methods=['GET'])
--- setup.py
+++ setup.py
@@ -6,16 +6,19 @@ import spotipy.util as util
 BOT_USERNAME = os.environ.get('MUSIK_USERNAME')
 BOT_API_KEY = os.environ.get('MUSIK_API_KEY')
 SPOTIFY_USERNAME = os.environ.get('SPOTIFY_USERNAME')
+SPOTIFY_CLIENT_ID = os.environ.get('SPOTIPY_CLIENT_ID')
+SPOTIFY_CLIENT_SECRET = os.environ.get('SPOTIPY_CLIENT_SECRET')
+SPOTIPY_REDIRECT_URI = os.environ.get('SPOTIPY_REDIRECT_URI')
 
 bot_config = {
 	"username" : BOT_USERNAME,
 	"key" : BOT_API_KEY
 }
 
-# spotify_scope = 'user-library-read'
+spotify_scope = 'user-library-read'
 
-# token = util.prompt_for_user_token(SPOTIFY_USERNAME, spotify_scope)
+token = util.prompt_for_user_token(SPOTIFY_USERNAME, spotify_scope, client_id=SPOTIFY_CLIENT_ID, client_secret= SPOTIFY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI)
 
-# sp = spotipy.Spotify(auth=token)
+sp = spotipy.Spotify(auth=token)
 
 kik = KikApi(bot_config["username"], bot_config["key"])
