c46725e92e6faab2499ac8300a6613288baea472
Marwan Ahmed <marwan.ahmed@kik.com>
2016-04-19 14:07:31 -0400

    force url for now

--- app/main/bot.py
+++ app/main/bot.py
@@ -31,7 +31,7 @@ def receive():
                     WubbleMessage(
                     to=message.from_user,
                     chat_id=message.chat_id,
-                    url=url_for("main.music_player", id="e001676375ea2b4807cee2f98b51f2f3fe0d109b", _external=True)
+                    url="https://songiq.herokuapp.com/musicplayer/53a95c27490ea1a42e0264a57fc73dacb961f2a7"
                 )
                     ])
                 return Response(status=200)
