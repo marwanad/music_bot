dfdca90ab3279382833c91d1d6794564fae8aa21
Marwan Ahmed <marwan.ahmed@kik.com>
2016-04-19 13:36:33 -0400

    add spotify client

--- app/main/music.py
+++ app/main/music.py
@@ -1,22 +1,11 @@
 import spotipy
-import spotipy.util as util
-
-scope = 'user-library-read'
-username = 'rfarmaha'
-
-token = util.prompt_for_user_token(username, scope)
-
-if token:
-    sp = spotipy.Spotify(auth=token)
-else:
-    print "Can't get token for {0}".format(username)
-
+from setup import sp
 
 def get_genres():
     return sp.recommendation_genre_seeds()['genres']
 
 
-def get_songs_from_genre(genre, difficulty=50):
+def get_song_from_genre(genre, difficulty=50):
     song = sp._get('recommendations', seed_genres=genre, limit=1, min_popularity=difficulty)
     if song:
         return song['tracks'][0]['preview_url']
--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 import os
 
 from kik import KikApi
+import spotipy.util as util
 
 BOT_USERNAME = os.environ.get('MUSIK_USERNAME')
 BOT_API_KEY = os.environ.get('MUSIK_API_KEY')
+SPOTIFY_USERNAME = os.environ.get('SPOTIFY_USERNAME')
 
 bot_config = {
 	"username" : BOT_USERNAME,
 	"key" : BOT_API_KEY
 }
+
+spotify_scope = 'user-library-read'
+
+token = util.prompt_for_user_token(SPOTIFY_USERNAME, scope)
+
+sp = spotipy.Spotify(auth=token)
 
 kik = KikApi(bot_config["username"], bot_config["key"])
