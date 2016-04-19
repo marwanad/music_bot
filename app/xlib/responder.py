633d1ec9661b306119ccaf81c529dc2e09e423c0
Marwan Ahmed <marwan.ahmed@kik.com>
2016-04-19 14:21:05 -0400

    suggested responses

--- app/xlib/responder.py
+++ app/xlib/responder.py
@@ -7,9 +7,8 @@ class Responder(object):
     @staticmethod
     def send_text_response(body, to, chat_id, keyboards=[]):
     	message = TextMessage(to=to, chat_id=chat_id, body=body)
-    	message.keyboards.append(SuggestedResponseKeyboard(
-    		to=to,
-    		hidden=True,
-    		responses=keyboards
-     ))
+    	# message.keyboards.append(SuggestedResponseKeyboard(
+    	# 	to=to,
+    	# 	hidden=True,
+    	# 	responses=keyboards))
     	kik.send_messages([message])
