734cccf60ae7e1980b5403dcbd34834095e899f2
rfarmaha <ramandeep.f@gmail.com>
2016-04-20 16:56:59 -0400

    Fixes for music.py

--- app/main/music.py
+++ app/main/music.py
@@ -6,37 +6,13 @@ import setup
 class Song:
     """Creates objects from Spotify music"""
 
-    def __init__(self):
-        self.album = None
-        self.artist = None
-        self.title = None
-        self.genre = None
-        self.album_art = None
-        self.preview_url = None
-
-    def with_album(self, album):
+    def __init__(self, album=None, artist=None, title=None, genre=None, album_art=None, preview_url=None):
         self.album = album
-        return self
-
-    def with_artist(self, artist):
         self.artist = artist
-        return self
-
-    def with_title(self, title):
         self.title = title
-        return self
-
-    def with_genre(self, genre):
         self.genre = genre
-        return self
-
-    def with_album_art(self, album_art):
         self.album_art = album_art
-        return self
-
-    def with_preview_url(self, preview_url):
         self.preview_url = preview_url
-        return self
 
 
 def refresh_spotify_client():
@@ -54,13 +30,12 @@ def get_song_from_genre(genre, difficulty=50):
     print ("getting song from genre ", genre)
     song_json = sp._get('recommendations', seed_genres=genre, limit=1, min_popularity=difficulty)['tracks'][0]
     if song_json:
-        final_song = (Song
-                      .with_album(song_json['album']['name'])
-                      .with_artist(song_json['artists'][0]['name'])
-                      .with_title(song_json['name'])
-                      .with_genre(genre)
-                      .with_album_art(song_json['album']['images'][1]['url'])
-                      .with_preview_url(_get_only_id(song_json['preview_url'])))
+        final_song = Song(song_json['album']['name'],
+                          song_json['artists'][0]['name'],
+                          song_json['name'],
+                          genre,
+                          song_json['album']['images'][1]['url'],
+                          _get_only_id(song_json['preview_url']))
         return final_song
     else:
         print 'Cannot get recommendation'
@@ -75,12 +50,13 @@ def get_song_from_artist(artist, difficulty=50):
         artist_id = artist['id']
         song_json = sp._get('recommendations', seed_artists=artist_id, limit=1, min_popularity=difficulty)['tracks'][0]
         if song_json:
-            song = (Song
-                    .with_album(song_json['album']['name'])
-                    .with_artist(song_json['artists'][0]['name'])
-                    .with_title(song_json['name'])
-                    .with_album_art(song_json['album']['images'][1]['url'])
-                    .with_preview_url(_get_only_id(song_json['preview_url'])))
+            song = Song(
+                    song_json['album']['name'],
+                    song_json['artists'][0]['name'],
+                    song_json['name'],
+                    None,
+                    song_json['album']['images'][1]['url'],
+                    _get_only_id(song_json['preview_url']))
             return song
         else:
             print 'Cannot parse song info'
