a6974209a1b6230a3d445005171bf6ee33631369
Shen Jin <sjin@Shens-Air.corp.kik.com>
2016-04-20 14:54:08 -0400

    fix for fallback state

--- app/handlers/handler.py
+++ app/handlers/handler.py
@@ -1,6 +1,7 @@
 from app.xlib.responder import Responder
 from app.xlib.game import StateType, Game
 from app.xlib.sr_strings import srs
+from app.xlib.states import StateString
 from ..main import music
 from ..decorators import check_state
 
@@ -8,69 +9,58 @@ from ..decorators import check_state
 class Handler(object):
     @staticmethod
     @check_state(StateType.INITIAL)
-    def handle_intro(to, chat_id):
-        body = 'Hi you reached the intro stage, tap a sr for more options'
+    def handle_intro(to, chat_id, body=StateString.INTRO):
         Responder.send_text_response(to, chat_id, body)
 
     @staticmethod
     @check_state(StateType.INITIAL)
-    def handle_start_quiz(to, chat_id):
-        body = 'Select genre, artist, or random'
+    def handle_start_quiz(to, chat_id, body=StateString.START_QUIZ):
         Game.get_game(chat_id).set_state(StateType.START_SELECT)
         Responder.send_text_response(to, chat_id, body, keyboards=srs.grouped_srs['song_options'])
 
     @staticmethod
     @check_state(StateType.START_SELECT)
-    def handle_genre(to, chat_id):
-        body = 'Select a genre'
+    def handle_genre(to, chat_id, body=StateString.GENRE):
         Game.get_game(chat_id).set_state(StateType.GENRE_SELECT)
         Responder.send_text_response(to, chat_id, body, keyboards=srs.grouped_srs['genre'])
         srs.register_sr('genre', 'handle_genre')
 
     @staticmethod
     @check_state(StateType.START_SELECT)
-    def handle_artist(to, chat_id):
-        body = 'Select an artist'
+    def handle_artist(to, chat_id, body=StateString.ARTIST):
         Game.get_game(chat_id).set_state(StateType.ARTIST_SELECT)
         Responder.send_text_response(to, chat_id, body, keyboards=srs.grouped_srs['artist'])
 
     @staticmethod
-    def handle_song(to, chat_id, song_id=None):
+    def handle_song(to, chat_id, song_id=None, body=StateString.SONG):
         track_preview_id = song_id
         if not track_preview_id:
             # grab a random song id (prob from popular playlist)
             track_preview_id = music.get_song_from_genre('pop')
 
-        body = 'Tap song above'
         Game.get_game(chat_id).set_state(StateType.ANSWER_TIME)
         Responder.send_wubble_response(to, chat_id, track_preview_id)
         Responder.send_text_response(to, chat_id, body, keyboards=srs.grouped_srs['menu'], hidden=True)
 
     @staticmethod
-    def handle_back(to, chat_id):
-        body = 'Ok, heading back'
+    def handle_back(to, chat_id, body=StateString.BACK):
         Game.get_game(chat_id).set_state(StateType.INITIAL)
         Responder.send_text_response(to, chat_id, body, keyboards=srs.grouped_srs['menu'])
 
     @staticmethod
-    def handle_custom_track(to, chat_id):
-        body = 'custom track'
+    def handle_share(to, chat_id, body=StateString.SHARE):
+        Game.get_game(chat_id).set_state(StateType.INITIAL)
         Responder.send_text_response(to, chat_id, body, keyboards=srs.grouped_srs['menu'])
 
     @staticmethod
-    def handle_share(to, chat_id):
-        body = 'share'
-        Responder.send_text_response(to, chat_id, body, keyboards=srs.grouped_srs['menu'])
-
-    @staticmethod
-    def handle_settings(to, chat_id):
-        body = 'settings'
+    def handle_settings(to, chat_id, body=StateString.SETTINGS):
+        Game.get_game(chat_id).set_state(StateType.INITIAL)
         Responder.send_text_response(to, chat_id, body, keyboards=srs.grouped_srs['menu'])
 
     @staticmethod
     def handle_fallback(to, chat_id, body=None):
         if body:
-            body = body.lower()
+            body = 'I don\'t understand what you mean by "{}"'.format(body)
         else:
             body = 'Not a text message'
 
