cb5dca8599739538db6f820e8d98bd46cd890be8
Marwan Ahmed <marwan.ahmed@kik.com>
2016-04-20 13:15:33 -0400

    db confg

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'w44xM94T2y')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'w44xM94T2y'
+    SQL_ALCHEMY_COMMIT_ON_TEARDOWN = True
     @staticmethod
     def init_app(app):
         pass
@@ -11,7 +12,8 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-
+    SQLALCHMEY_DATABASE_URI = 'postgres://' + os.environ.get('SONGIQ_DB_ADMIN') + ':' 
+    os.environ.get('SONGIQ_DB_PASS') + '@' + os.environ.get('SONGIQ_DB_HOST')
 
 class ProductionConfig(Config):
     pass
