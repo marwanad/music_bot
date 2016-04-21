2e58f786e715fcd60b9ca72015a3958e0f570924
Marwan Ahmed <marwan.ahmed@kik.com>
2016-04-21 18:33:23 -0400

    use fuzzy

--- util.py
+++ util.py
@@ -1,10 +1,10 @@
 import string
 import re
-
+from fuzzywuzzy import fuzz
 
 def guess_matches_answer(guess, answer):
     regex = re.compile('[%s]' % re.escape(string.punctuation))
     guess_no_punc = regex.sub('', guess).strip().lower()
     answer_no_punc = regex.sub('', answer).strip().lower()
-    return guess_no_punc == answer_no_punc
+    return fuzz.ratio(guess_no_punc, answer_no_punc) >= 95
 
