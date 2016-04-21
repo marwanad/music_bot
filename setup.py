845741339fb40b9b8e05725c0fb364a534bd3bfe
s29jin <s29jin@uwaterloo.ca>
2016-04-21 00:01:01 -0400

    get rid of spotipy

--- setup.py
+++ setup.py
@@ -2,31 +2,29 @@ import os
 
 from kik import KikApi
 import requests, base64
-import spotipy.util as util
-import spotipy
 
 BOT_USERNAME = os.environ.get('MUSIK_USERNAME')
 BOT_API_KEY = os.environ.get('MUSIK_API_KEY')
 token_response_json = None
 
 bot_config = {
-	"username" : BOT_USERNAME,
-	"key" : BOT_API_KEY
+    "username": BOT_USERNAME,
+    "key": BOT_API_KEY
 }
 
 kik = KikApi(bot_config["username"], bot_config["key"])
 
+
 def get_spotify_token():
-	spotify_scope = 'user-library-read'
-	SPOTIFY_USERNAME = os.environ.get('SPOTIFY_USERNAME')
-	token_data = {
-		'grant_type':'refresh_token',
-		'refresh_token': os.environ.get('SPOTIPY_REFRESH_TOKEN'),
-		}
-	
-	clientID = os.environ.get('SPOTIPY_CLIENT_ID') + ":" + os.environ.get('SPOTIPY_CLIENT_SECRET')
-	b64Val = base64.b64encode(clientID)
-	r = requests.post('https://accounts.spotify.com/api/token', headers={'Authorization': 'Basic ' + b64Val}, data=token_data)
-	
-	token_response_json = r.json()
-	return token_response_json['access_token'];
+    token_data = {
+        'grant_type': 'refresh_token',
+        'refresh_token': os.environ.get('SPOTIPY_REFRESH_TOKEN'),
+    }
+
+    clientID = os.environ.get('SPOTIPY_CLIENT_ID') + ":" + os.environ.get('SPOTIPY_CLIENT_SECRET')
+    b64Val = base64.b64encode(clientID)
+    r = requests.post('https://accounts.spotify.com/api/token', headers={'Authorization': 'Basic ' + b64Val},
+                      data=token_data)
+
+    token_response_json = r.json()
+    return token_response_json['access_token'];
