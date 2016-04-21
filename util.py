cb583634332847feca72592380242b926a531afa
rfarmaha <ramandeep.f@gmail.com>
2016-04-21 16:00:00 -0400

    Fix

--- util.py
+++ util.py
@@ -1,6 +1,7 @@
 import string
 
 def guess_matches_answer(guess, answer):
-    guess_no_punc = guess.translate(string.maketrans("", ""), string.punctuation).strip().lower()
-    answer_no_punc = answer.translate(string.maketrans("", ""), string.punctuation).strip().lower()
+    remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
+    guess_no_punc = guess.translate(string.maketrans("", ""), remove_punctuation_map).strip().lower()
+    answer_no_punc = answer.translate(string.maketrans("", ""), remove_punctuation_map).strip().lower()
     return guess_no_punc == answer_no_punc
