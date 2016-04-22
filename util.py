49e3df9c61c531a82a4d319bc27a5a0b07a7b0b0
Marwan Ahmed <marwan.ahmed@kik.com>
2016-04-22 11:24:42 -0400

    remove dash

--- util.py
+++ util.py
@@ -5,7 +5,7 @@ from fuzzywuzzy import fuzz
 def guess_matches_answer(guess, answer):
     regex = re.compile('[%s]' % re.escape(string.punctuation))
     guess_no_punc = regex.sub('', guess).strip().lower()
-    guess_no_dash = guess_no_punc.split('-')[0]
     answer_no_punc = regex.sub('', answer).strip().lower()
-    return fuzz.ratio(guess_no_dash, answer_no_punc) >= 95
+    answer_no_dash = answer_no_punc.split('-')[0]
+    return fuzz.ratio(guess_no_dash, answer_no_dash) >= 90
 
