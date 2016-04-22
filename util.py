b15401197643aabcc47206b4f5b62720409cb216
Marwan Ahmed <marwan.ahmed@kik.com>
2016-04-22 11:26:43 -0400

    no punk

--- util.py
+++ util.py
@@ -7,5 +7,5 @@ def guess_matches_answer(guess, answer):
     guess_no_punc = regex.sub('', guess).strip().lower()
     answer_no_punc = regex.sub('', answer).strip().lower()
     answer_no_dash = answer_no_punc.split('-')[0]
-    return fuzz.ratio(guess_no_dash, answer_no_dash) >= 90
+    return fuzz.ratio(guess_no_punc, answer_no_dash) >= 90
 
