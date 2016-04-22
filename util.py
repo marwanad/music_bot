7d6a3332da72833b9625cd338404c20037335e9c
Marwan Ahmed <marwan.ahmed@kik.com>
2016-04-22 14:48:26 -0400

    log util

--- util.py
+++ util.py
@@ -7,5 +7,8 @@ def guess_matches_answer(guess, answer):
     guess_no_punc = regex.sub('', guess).strip().lower()
     answer_no_punc = regex.sub('', answer).strip().lower()
     answer_no_dash = answer_no_punc.split('-')[0]
+    print("Answer without dash is ", answer_no_dash)
+    print("Guess without punct is", guess_no_punc)
+    
     return fuzz.ratio(guess_no_punc, answer_no_dash) >= 90
 
