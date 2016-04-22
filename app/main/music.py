789fb70d74a5edfc5ae2ebee490d9f09f3ac8002
marwanad <mrwnad@gmail.com>
2016-04-22 01:32:42 -0400

    better handling for init client

--- app/main/music.py
+++ app/main/music.py
@@ -6,12 +6,13 @@ import setup
 
 preview_base_url="https://p.scdn.co/mp3-preview/"
 
-sp = spotipy.Spotify(auth=setup.get_spotify_token())
+sp = refresh_spotify_client()
 
 def refresh_spotify_client():
     if(setup.is_cached_token_valid()):
         print("Called before request and found access token to be valid")
-        return
+        # return same instance if not none or new instance with token
+        return sp if not None or sp = spotipy.Spotify(auth=setup.get_spotify_token())
     sp = spotipy.Spotify(auth=setup.get_spotify_token())
 
 
